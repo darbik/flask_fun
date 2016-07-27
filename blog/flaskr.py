09d470927cc1dd537a2e53abe8d98e5a0bf8ac59
darbik <dfesik1@gmail.com>
2016-07-27 18:21:50 +0000

    Added further database functionality for getting and closing the database.

--- blog/flaskr.py
+++ blog/flaskr.py
@@ -21,3 +21,19 @@ def connect_db():
 
     return rv
 
+def get_db():
+
+    if not hasattr(g, 'sqlite_db'):
+
+        g.sqlite_db = connect_db()
+
+    return g.sqlite_db
+
+@app.teardown_appcontext
+    
+def close_db(error):
+
+    if hasattr(g, 'sqlite_db'):
+
+        g.sqlite_db.close()
+
