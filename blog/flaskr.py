9488069b45ad1db68e29848a4997d69af563b163
darbik <dfesik1@gmail.com>
2016-07-27 16:32:07 +0000

    Added a connect to database function.

--- blog/flaskr.py
+++ blog/flaskr.py
@@ -14,3 +14,10 @@ app.config.update(dict(
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def connect_db():
+
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+
+    return rv
+
