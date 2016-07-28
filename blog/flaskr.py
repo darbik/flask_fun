45337583d8d6fefff37f167a5db21c2a9fbda73f
darbik <dfesik1@gmail.com>
2016-07-28 15:34:18 +0000

    Added a database initialization command line script.

--- blog/flaskr.py
+++ blog/flaskr.py
@@ -21,6 +21,24 @@ def connect_db():
 
     return rv
 
+def init_db():
+
+    db = get_db()
+
+    with app.open_resource('schema.sql', mode = 'r') as f:
+
+        db.cursor().executescript(f.read())
+
+    db.commit()
+
+@app.cli.command('initdb')
+
+def initdb_command():
+
+    init_db()
+
+    print "Initialized the database."
+
 def get_db():
 
     if not hasattr(g, 'sqlite_db'):
