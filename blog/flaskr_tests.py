2cae153972e75acf57a16a8a7778d59f6b9584b6
darbik <dfesik1@gmail.com>
2016-08-03 16:34:07 +0000

    Added a test_empty_db() function.

--- blog/flaskr_tests.py
+++ blog/flaskr_tests.py
@@ -8,9 +8,8 @@ class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
 
         self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
         self.app = flaskr.app.test_client()
-
+        
         with flaskr.app.app_context():
 
             flaskr.init_db()
@@ -19,7 +18,12 @@ class FlaskrTestCase(unittest.TestCase):
 
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
+    
+    def test_empty_db(self):
 
+        rv = self.app.get('/')
+        assert b'No entries here so far.' in rv.data
+        
 if __name__  == '__main__':
 
     unittest.main()
